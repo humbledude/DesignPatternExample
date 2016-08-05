57166fe1371b482552dbc55a3b6f15e5f1d4d80b
humbledude <keunhui.park@gmail.com>
2016-08-05 13:55:02 +0900

    strategy1 template

---
+++ strategy1/src/main/java/com/humbledude/examples/strategy1/App.java
@@ -0,0 +1,11 @@
+package com.humbledude.examples.strategy1;
+
+/**
+ * Created by keunhui.park on 2016. 8. 5..
+ */
+public class App {
+
+  public static void main(String[] args) {
+    System.out.print("hello world");
+  }
+}
